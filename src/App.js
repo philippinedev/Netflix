a7d7dbc77a592ccfbb2b5e9613fe26d50ffcec5f
Raymond <raymond@philippinedev.com>
2022-01-12 02:02:17 +0800

    created banner section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Building Netflix Clone</h1>
+    <div className="app">
+      <Banner />
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
---
+++ src/Banner.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react"
+import axios from "./axios"
+import requests from "./requests"
+import "./Banner.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Banner() {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      const randMovie = Math.floor(Math.random() * request.data.results.length -1)
+      setMovie((request.data.results[randMovie]))
+      return request
+    }
+    fetchData()
+  }, [])
+
+  console.log(movie)
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str
+  }
+
+  return (
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(${baseUrl}${movie?.backdrop_path})`,
+        backgroundPosition: "center center"
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">More Info</button>
+        </div>
+
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 160)}
+        </h1>
+      </div>
+
+      <div className="banner_fade"></div>
+    </header>
+  )
+}
+
+export default Banner
